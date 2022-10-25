738cf96bbd121bcac1496aaf171c1bf9a999a4dc
rico45n <rpesnin@inbox.ru>
2022-10-25 08:15:13 +0300

    Initial commit

---
+++ src/main/java/com/example/TestChat/TestChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TestChat/TestChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
